422ea6d3114a645741c434ee2bade6406f3e1c56
ceti <ceti@TartangaLH.eus>
2025-11-04 16:36:15 +0100

    fibo.php

---
+++ fibo.php
@@ -0,0 +1,48 @@
+<?php
+    
+    //Alex Garcia Pliego
+    
+    function fibonacci ($var) {
+
+        // Inicializar array que guardala secuencia. 
+        $secuencia = [0,1];
+
+        // Bucle con el que se asigna cada valor de la secuencia a su índice en el array. Comienza en el índice 1, y se repite hasta que la diferencia entre este y el parámetro sea de uno, siendo el índice menor que el parámetro.
+        for ($i = 1; $i < ($var - 1); $i++) {
+            
+            // Asignar a cada índice su valor.
+            $secuencia[($i+1)] = $secuencia[$i] + $secuencia[($i-1)];
+    
+        }
+
+        // En caso de que el parámetro sea 1, se devolverá un único valor (el 0, el primero de la secuencia).
+        if ($var == 1) {
+        
+            $secuencia = [0];
+        
+        }
+
+        // Devolver la secuencia.
+        return $secuencia;
+    }
+
+    // Parámetro hasta el que se imprime la secuencia de Fibonacci.
+    $valor = 6;
+    if ($valor <= 0){
+        
+        echo "El valor no es adecuado para la secuencia de Fibonacci.";
+    
+    } else {
+        // Array que guarda la secuencia de Fibonacci.
+        $arr = fibonacci($valor);
+        
+        echo "La secuencia de Fibonacci hasta el valor $valor es la siguiente: <br>";
+    
+        for ($i = 0; $i <= $valor; $i++){
+            
+            echo $arr[$i] . "<br>";
+            
+        }
+    }
+    
+?>
